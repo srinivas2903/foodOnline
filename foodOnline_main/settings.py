6cf40d46e1673557b0c680bf630e0cacc7a8cb5a
srinivas <srinivasdevu474@gmail.comg>
2024-04-28 18:31:02 +0530

    register user functionalities and the homepage setup

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -83,6 +85,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL='accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -124,7 +127,17 @@ STATICFILES_DIRS= [
     'foodOnline_main/static'
 ]
 
+#media files configurations
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS={
+    messages.ERROR:'danger',
+}
